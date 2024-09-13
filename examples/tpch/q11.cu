9469ace9bfd62773f977ceca00a08b3bb80f034e
Avinash Jayakar <avithemad@gmail.com>
2024-09-13 19:34:42 +0530

    Handle string like columns

--- examples/tpch/q11.cu
+++ examples/tpch/q11.cu
@@ -44,8 +44,15 @@ o_shippriority: int64
 o_comment: large_string
  */
 
+struct StringColumn {
+  int* sizes;
+  char** stringAddresses;
+  char* data;
+  StringColumn() {}
+};
+
 template<typename T>
-T* read_column (std::shared_ptr<arrow::Table> &table, std::string column) {
+T* read_column (std::shared_ptr<arrow::Table> &table, const std::string &column) {
   // TODO: add error handling for column not present in the schema
   T* carr = (T*)malloc(sizeof(T)*table->num_rows());
   auto arrow_col = table->GetColumnByName(column);
@@ -58,6 +65,41 @@ T* read_column (std::shared_ptr<arrow::Table> &table, std::string column) {
     }
   }
   return carr;
+}
+
+StringColumn* read_string_column(std::shared_ptr<arrow::Table> &table, const std::string &column) {
+  //TODO: add all kinds of error handling
+  auto arrow_col = table->GetColumnByName(column);
+  StringColumn* result = new StringColumn();
+
+  // calculate the size of data
+  result->sizes = (int*)malloc(sizeof(int)*table->num_rows());
+  result->stringAddresses = (char**)malloc(sizeof(char*)*table->num_rows());
+  int data_size = 0;
+  int j=0;
+  for (auto chunk: arrow_col->chunks()) {
+    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
+    for (int i=0; i < string_arr->length(); i++) {
+      auto str = string_arr->GetString(i);
+      data_size += str.size();
+      result->sizes[j++] = str.size();
+    }
+  }
+  result->data = (char*)malloc(sizeof(char)*data_size);
+  j = 0;
+  char* straddr = result->data;
+  for (auto chunk: arrow_col->chunks()) {
+    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
+    for (int i=0; i < string_arr->length(); i++) {
+      auto str = string_arr->GetString(i);
+      result->stringAddresses[i] = straddr;
+      straddr += str.size();
+      for (auto c: str) {
+        result->data[j++] = c;
+      }
+    }
+  }
+  return result;
 }
     
 arrow::Status read_parquet(std::string path_to_file, std::shared_ptr<arrow::Table> &table) {
@@ -84,4 +126,14 @@ int main() {
         std::cerr << "Some error in reading file\n" << st.ToString();
     }
     int64_t* l_orderkey = read_column<int64_t>(table, "l_orderkey");
+    auto l_shipmode = read_string_column(table, "l_shipmode");
+
+    for (int i=0; i<table->num_rows(); i++) {
+      std::cout << l_shipmode->sizes[i];
+      std::cout << std::endl;
+      for (int j=0; j<l_shipmode->sizes[i]; j++) {
+        std::cout << l_shipmode->stringAddresses[i][j];
+      }
+      std::cout << std::endl;
+    }
 }
