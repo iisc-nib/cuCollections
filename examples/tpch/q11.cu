efd80c26e2d95db269aba8d23dfa5957ed2b14a3
Avinash Jayakar <avithemad@gmail.com>
2024-09-15 18:35:11 +0530

    Crude implementation of tpch q3

--- examples/tpch/q11.cu
+++ examples/tpch/q11.cu
@@ -14,6 +14,7 @@
 #include <cooperative_groups.h>
 #include <cuco/static_map.cuh>
 #include <cuda.h>
+#include "utils.h"
 
 /**
  * c_custkey: int64
@@ -52,147 +53,87 @@ o_clerk: large_string
 o_shippriority: int64
 o_comment: large_string
  */
+CUCO_DECLARE_BITWISE_COMPARABLE(double);
 
+struct agg_key_type {
+  int64_t l_orderkey;
+  int32_t o_orderdate;
+  int64_t o_shippriority;
+  __host__ __device__ agg_key_type() {}
+  __host__ __device__ agg_key_type(int32_t x): l_orderkey(x), o_orderdate(x), o_shippriority(x) {}
+};
 #define TILE_SIZE 1
 namespace cg = cooperative_groups;
 template <typename Map>
-__global__ void build_hash(Map map_ref, int64_t* column, int32_t* filter, size_t column_size) {
+__global__ void build_hash_order(Map map_ref, int64_t* column, int32_t* filter, size_t column_size) {
+  int tid = (threadIdx.x + blockIdx.x*blockDim.x) ;
+  auto this_thread = cg::tiled_partition<TILE_SIZE>(cg::this_thread_block());
+  
+  if (tid >= column_size) return;
+  if (filter[tid] >= 9204) return;
+  
+  map_ref.insert(this_thread, cuco::pair{column[tid], tid});
+}
+
+template <typename Map>
+__global__ void build_hash_customer(Map map_ref, int64_t* column, int32_t* filter, size_t column_size, int32_t building_code) {
   int tid = (threadIdx.x + blockIdx.x*blockDim.x) / TILE_SIZE;
   auto this_thread = cg::tiled_partition<TILE_SIZE>(cg::this_thread_block());
   
   if (tid >= column_size) return;
-  if (filter[tid] < 9204) return;
+  if (filter[tid] != building_code) return;
   map_ref.insert(this_thread, cuco::pair{column[tid], tid});
 }
 
-struct StringColumn {
-  int* sizes;
-  char** stringAddresses;
-  char* data;
-  StringColumn() {}
+template <typename Map, typename AggMap>
+__global__ void probe(Map order_hash,
+                      Map customer_hash,
+                      int32_t* l_shipdate,
+                      int64_t* l_orderkey,
+                      int64_t* o_custkey,
+                      int32_t* o_orderdate,
+                      int64_t* o_shippriority,
+                      double* l_extendedprice,
+                      double* l_discount,
+                      size_t l_size, size_t o_size,
+                      double* result, AggMap agg_map) {
+  int tid = (threadIdx.x + blockIdx.x*blockDim.x) / TILE_SIZE;
+  if (tid >= l_size) return;
+
+  // filter line item by shipdate
+  if (l_shipdate[tid] <= 9204) return; 
+  auto order_idx = order_hash.find(l_orderkey[tid]);
+  if (order_idx == order_hash.end()) return;
+  auto cust_idx = customer_hash.find(o_custkey[order_idx->second]);
+  if (cust_idx == customer_hash.end()) return;
+  //printf("%f\n", l_extendedprice[tid]); 
+  // result[l_orderkey[tid]%l_size] += l_extendedprice[tid];
+  //atomicAdd(result+(l_orderkey[tid]%l_size), l_extendedprice[tid]);
+  //group by o_orderdate, l_orderkey, o_shippriority
+  // prepare the packed key
+  int64_t aggkey = 0;
+  aggkey |= l_orderkey[tid];
+  aggkey |= (o_orderdate[order_idx->second] << 32);
+  double res = l_extendedprice[tid]*(1.0f - l_discount[tid]);
+  auto [slot, is_new_key]= agg_map.insert_and_find(cuco::pair{aggkey, res});
+  if (!is_new_key) {
+    auto ref = cuda::atomic_ref<typename AggMap::mapped_type, cuda::thread_scope_device>{slot->second};
+    ref.fetch_add(res, cuda::memory_order_relaxed);
+  }
+}
+
+struct floatbitwise : cuco::is_bitwise_comparable<double> {
+  double val;
+  floatbitwise(double v) : val(v){}
 };
-struct StringDictEncodedColumn {
-  std::unordered_map<std::string, int32_t> dict;
-  int32_t* column;
-};
-
-template<typename T>
-T* read_column (std::shared_ptr<arrow::Table> &table, const std::string &column) {
-  // TODO: add error handling for column not present in the schema
-  T* carr = (T*)malloc(sizeof(T)*table->num_rows());
-  auto arrow_col = table->GetColumnByName(column);
-  for (auto chunk: arrow_col->chunks()) {
-    if (std::is_same<T, int64_t>::value) {
-      auto intArr = std::static_pointer_cast<arrow::Int64Array>(chunk);
-      for (int i=0; i<intArr->length(); i++) {
-        carr[i] = intArr->Value(i);
-      }
-    } else if (std::is_same<T, int32_t>::value) { // use int32 for date32 for now
-      // date32 type represents the number of days since UNIX epoch 1970-01-01
-      auto dateArr = std::static_pointer_cast<arrow::Date32Array>(chunk);
-
-      for (int i=0; i<dateArr->length(); i++) {
-        carr[i] = dateArr->Value(i);
-      }
-    } else if (std::is_same<T, double>::value) {
-      auto doubleArr = std::static_pointer_cast<arrow::DoubleArray>(chunk);
-      for (int i=0; i<doubleArr->length(); i++) {
-       carr[i] = doubleArr->Value(i);
-      }
-    }
-  }
-  return carr;
-}
-
-StringDictEncodedColumn* read_string_dict_encoded_column(std::shared_ptr<arrow::Table> &table, const std::string &column) {
-  auto arrow_col = table->GetColumnByName(column);
-  StringDictEncodedColumn* result = new StringDictEncodedColumn();
-  int32_t uid = 0;
-  for (auto chunk: arrow_col->chunks()) {
-    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
-    for (int i=0; i < string_arr->length(); i++) {
-      auto str = string_arr->GetString(i);
-      if (result->dict.find(str) == result->dict.end()) {
-        result->dict[str] = uid; uid++;
-      }
-    }
-  }
-  result->column = new int32_t[table->num_rows()];
-  int j = 0; 
-  for (auto chunk: arrow_col->chunks()) {
-    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
-    for (int i=0; i < string_arr->length(); i++) {
-      auto str = string_arr->GetString(i);
-      result->column[j++] = result->dict[str];
-    }
-  }
-  return result;
-}
-StringColumn* read_string_column(std::shared_ptr<arrow::Table> &table, const std::string &column) {
-
-  //TODO: add all kinds of error handling
-  auto arrow_col = table->GetColumnByName(column);
-  StringColumn* result = new StringColumn();
-
-  // calculate the size of data
-  result->sizes = (int*)malloc(sizeof(int)*table->num_rows());
-  result->stringAddresses = (char**)malloc(sizeof(char*)*table->num_rows());
-  int data_size = 0;
-  int j=0;
-  for (auto chunk: arrow_col->chunks()) {
-    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
-    for (int i=0; i < string_arr->length(); i++) {
-      auto str = string_arr->GetString(i);
-      data_size += str.size();
-      result->sizes[j++] = str.size();
-    }
-  }
-  result->data = (char*)malloc(sizeof(char)*data_size);
-  j = 0;
-  char* straddr = result->data;
-  for (auto chunk: arrow_col->chunks()) {
-    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
-    for (int i=0; i < string_arr->length(); i++) {
-      auto str = string_arr->GetString(i);
-      result->stringAddresses[i] = straddr;
-      straddr += str.size();
-      for (auto c: str) {
-        result->data[j++] = c;
-      }
-    }
-  }
-  return result;
-}
-    
-arrow::Status read_parquet(std::string path_to_file, std::shared_ptr<arrow::Table> &table) {
-  arrow::MemoryPool* pool = arrow::default_memory_pool();
-  // open file
-  std::shared_ptr<arrow::io::RandomAccessFile> input;
-  ARROW_ASSIGN_OR_RAISE(input, arrow::io::ReadableFile::Open(path_to_file));
-  // initialize file reader
-  std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
-  ARROW_RETURN_NOT_OK(parquet::arrow::OpenFile(input, pool, &arrow_reader));
-
-  ARROW_RETURN_NOT_OK(arrow_reader->ReadTable(&table));
-
-  return arrow::Status::OK();
-}
- 
-std::shared_ptr<arrow::Table> getArrowTable(std::string path_to_file) {
-  std::shared_ptr<arrow::Table> lineitem_table;
-  arrow::Status st = read_parquet(path_to_file, lineitem_table);
-  if (st != arrow::Status::OK()) {
-    std::cerr << st.ToString();
-    return nullptr;
-  }
-  return lineitem_table;
-}
-
 int main() {
   std::string dbDir = "/media/ajayakar/space/src/tpch/data/tables/scale-10.0/";
   std::string lineitem_file = dbDir + "lineitem.parquet";
   std::string orders_file = dbDir + "orders.parquet";
   std::string customer_file = dbDir + "customer.parquet";
+
+  auto const agg_empty_key_sentinel = agg_key_type{-1};
+
 
   auto lineitem_table = getArrowTable(lineitem_file);
   size_t lineitem_size = lineitem_table->num_rows();
@@ -215,24 +156,119 @@ int main() {
 
   int32_t* c_custkey = read_column<int32_t>(customer_table, "c_custkey");
   StringDictEncodedColumn* c_mktsegment = read_string_dict_encoded_column(customer_table, "c_mktsegment");
+  int32_t building_code = c_mktsegment->dict["BUILDING"];
+  //for (auto e: c_mktsegment->dict) std::cout << e.first << " -> " << e.second << std::endl;
 
-  std::size_t const capacity = orders_size*2;
   auto o_orderkey_map = cuco::static_map{
-    capacity,
-    cuco::empty_key{-1},
-    cuco::empty_value{-1},
-    thrust::equal_to<int>{},
-    cuco::linear_probing<TILE_SIZE, cuco::default_hash_function<int>>()
+    orders_size*2,
+    cuco::empty_key{(int64_t)-1},
+    cuco::empty_value{(int64_t)-1},
+    thrust::equal_to<int64_t>{},
+    cuco::linear_probing<TILE_SIZE, cuco::default_hash_function<int64_t>>()
   };  
-  int64_t *d_o_orderkey; int32_t *d_o_orderdate;
+  auto c_custkey_map = cuco::static_map{
+    customer_size*2,
+    cuco::empty_key{(int64_t)-1},
+    cuco::empty_value{(int64_t)-1},
+    thrust::equal_to<int64_t>{},
+    cuco::linear_probing<TILE_SIZE, cuco::default_hash_function<int64_t>>()
+  };  
+  auto agg_map = cuco::static_map{customer_size*2,
+    cuco::empty_key{(int64_t)-1},
+    cuco::empty_value{(double)-1},
+    thrust::equal_to<int64_t>{},
+    cuco::linear_probing<TILE_SIZE, cuco::default_hash_function<int64_t>>()
+  };
+  int64_t *d_o_orderkey, *d_c_custkey, *d_l_orderkey, *d_o_custkey, *d_o_shippriority; 
+  int32_t *d_o_orderdate, *d_c_mktsegment, *d_l_shipdate, *d_l_orderdate;
+  double *d_l_extendedprice, *d_l_discount;
   cudaMalloc(&d_o_orderkey, orders_size*sizeof(int64_t));
+  cudaMalloc(&d_c_custkey, customer_size*sizeof(int64_t));
+  cudaMalloc(&d_l_orderkey, lineitem_size*sizeof(int64_t));
+  cudaMalloc(&d_o_custkey, orders_size*sizeof(int64_t));
+  cudaMalloc(&d_o_shippriority, orders_size*sizeof(int64_t));
   cudaMalloc(&d_o_orderdate, orders_size*sizeof(int32_t));
+  cudaMalloc(&d_c_mktsegment, customer_size*sizeof(int32_t));
+  cudaMalloc(&d_l_shipdate, lineitem_size*sizeof(int32_t));
+  cudaMalloc(&d_l_extendedprice, lineitem_size*sizeof(double));
+  cudaMalloc(&d_l_discount, lineitem_size*sizeof(double));
+
   cudaMemcpy(d_o_orderkey, o_orderkey, orders_size*sizeof(int64_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_c_custkey, c_custkey, customer_size*sizeof(int64_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_l_orderkey, l_orderkey, lineitem_size*sizeof(int64_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_o_custkey, o_custkey, orders_size*sizeof(int64_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_o_shippriority, o_shippriority, orders_size*sizeof(int64_t), cudaMemcpyHostToDevice);
   cudaMemcpy(d_o_orderdate, o_orderdate, orders_size*sizeof(int32_t), cudaMemcpyHostToDevice);
-  build_hash<<<(int)std::ceil(((float)orders_size)/1024.0), 1024>>> (
+  cudaMemcpy(d_c_mktsegment, c_mktsegment->column, customer_size*sizeof(int32_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_l_shipdate, l_shipdate, lineitem_size*sizeof(int32_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_l_extendedprice, l_extendedprice, lineitem_size*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_l_discount, l_discount, lineitem_size*sizeof(double), cudaMemcpyHostToDevice);
+
+  int threadBlockSize = 1024;
+  build_hash_order<<<getGridSize(orders_size, threadBlockSize), threadBlockSize>>> (
     o_orderkey_map.ref(cuco::insert),
     d_o_orderkey,
     d_o_orderdate,
     orders_size
   );
+  build_hash_customer<<<getGridSize(customer_size, threadBlockSize), threadBlockSize>>> (
+    c_custkey_map.ref(cuco::insert),
+    d_c_custkey,
+    d_c_mktsegment,
+    customer_size,
+    building_code
+  );
+  size_t res_size = lineitem_size;
+  double* res = new double[res_size];
+  for (int i=0; i<res_size; i++) res[i] = 0;
+  double *d_res;
+  cudaMalloc(&d_res, lineitem_size*sizeof(double));
+  cudaMemcpy(d_res, res, lineitem_size*sizeof(double), cudaMemcpyHostToDevice);
+  probe<<<getGridSize(lineitem_size, threadBlockSize), threadBlockSize>>> (
+    o_orderkey_map.ref(cuco::find),
+    c_custkey_map.ref(cuco::find),
+    d_l_shipdate,
+    d_l_orderkey,
+    d_o_custkey,
+    d_o_orderdate,
+    d_o_shippriority,
+    d_l_extendedprice,
+    d_l_discount,
+    lineitem_size, 
+    orders_size,
+    d_res, 
+    agg_map.ref(cuco::insert_and_find));
+  int agg_map_size = agg_map.size();
+  std::cout << agg_map.capacity() << "| map size: " << agg_map_size << std::endl;
+  thrust::device_vector<double> result_rev(agg_map_size);
+  thrust::device_vector<int64_t> result_keys(agg_map_size);
+  agg_map.retrieve_all(result_keys.begin(), result_rev.begin());
+
+  std::vector<double> rev;
+  for (int i=0; i<agg_map_size; i++) {
+  //   std::cout << result_keys[i] << " : " << result_rev[i] << std::endl;
+    rev.push_back(result_rev[i]);
+  }
+  std::sort(rev.rbegin(), rev.rend());
+  std::cout << "Printing the first 10 sorted revenues:\n";
+  for (int i=0; i<10; i++) {
+    std::cout << rev[i] << std::endl;
+  }
+  // cudaMemcpy(res, d_res, lineitem_size*sizeof(double),cudaMemcpyDeviceToHost);
+  // std::vector<double> r;
+  // for (int i=0; i<lineitem_size; i++) {
+  //   if (res[i]!=0) r.push_back(res[i]);
+  // }
+  // std::sort(r.rbegin(), r.rend());
+  // for (int i=0; i<10; i++) {
+  //   std::cout << r[i] ;
+  //   std::cout << "\t" << l_extendedprice[i] << std::endl;
+  // }
+  // print out the max and min values of o_orderdate, o_shippriority and l_orderkey to see if it can be fit as a composite key of 32 bit
+  // int32_t lok_min = INT_MAX, lok_max = INT_MIN;
+  // for (int i=0; i<orders_size; i++) {
+  //   lok_min = std::min(lok_min, o_orderdate[i]);
+  //   lok_max = std::max(lok_max, o_orderdate[i]);
+  // }
+  // std::cout << "l_orderkey: min=" << lok_min << " max=" << lok_max << std::endl;
 }
---
+++ examples/tpch/utils.h
@@ -0,0 +1,163 @@
+#include <unordered_map>
+
+#include <arrow/io/api.h>
+#include <arrow/table.h>
+#include <arrow/record_batch.h>
+#include <arrow/array.h>
+#include <iostream>
+#include <parquet/arrow/reader.h>
+
+struct StringColumn {
+  int* sizes;
+  char** stringAddresses;
+  char* data;
+  StringColumn() {}
+};
+struct StringDictEncodedColumn {
+  std::unordered_map<std::string, int32_t> dict;
+  int32_t* column;
+};
+
+template<typename T>
+T* read_column (std::shared_ptr<arrow::Table> &table, const std::string &column) {
+  // TODO: add error handling for column not present in the schema
+  T* carr = (T*)malloc(sizeof(T)*table->num_rows());
+  auto arrow_col = table->GetColumnByName(column);
+  for (auto chunk: arrow_col->chunks()) {
+    if (std::is_same<T, int64_t>::value) {
+      auto intArr = std::static_pointer_cast<arrow::Int64Array>(chunk);
+      for (int i=0; i<intArr->length(); i++) {
+        carr[i] = intArr->Value(i);
+      } 
+    } else if (std::is_same<T, int32_t>::value) { // use int32 for date32 for now
+    // TODO: come up with better type castings
+      // date32 type represents the number of days since UNIX epoch 1970-01-01
+      auto dateArr = std::static_pointer_cast<arrow::Date32Array>(chunk);
+      for (int i=0; i<dateArr->length(); i++) {
+        carr[i] = dateArr->Value(i);
+      }
+    } else if (std::is_same<T, int16_t>::value) {
+    // TODO: come up with better type castings
+      // same for this
+      auto dateArr = std::static_pointer_cast<arrow::Date32Array>(chunk);
+
+      for (int i=0; i<dateArr->length(); i++) {
+        carr[i] = (T)dateArr->Value(i);
+      }
+    } 
+    else if (std::is_same<T, double>::value) {
+      auto doubleArr = std::static_pointer_cast<arrow::DoubleArray>(chunk);
+      for (int i=0; i<doubleArr->length(); i++) {
+       carr[i] = doubleArr->Value(i);
+      }
+    }
+  }
+  return carr;
+}
+
+template<typename T>
+T* read_column_typecasted(std::shared_ptr<arrow::Table> &table, const std::string &column) {
+  T* carr = (T*)malloc(sizeof(T)*table->num_rows());
+  auto arrow_col = table->GetColumnByName(column);
+  for (auto chunk: arrow_col->chunks()) {
+    if (std::is_same<T, int32_t>::value || std::is_same<T, int16_t>::value) {
+      auto arr = std::static_pointer_cast<arrow::Int64Array>(chunk);
+      for (int i=0; i<arr->length(); i++) {
+        carr[i] = (T)arr->Value(i);
+      }
+    } else if (std::is_same<T, float>::value) {
+      auto arr = std::static_pointer_cast<arrow::DoubleArray>(chunk);
+      for (int i=0; i<arr->length(); i++) {
+        carr[i] = (T)arr->Value(i);
+      }
+    }
+  }
+  return carr;
+}
+
+StringDictEncodedColumn* read_string_dict_encoded_column(std::shared_ptr<arrow::Table> &table, const std::string &column) {
+  auto arrow_col = table->GetColumnByName(column);
+  StringDictEncodedColumn* result = new StringDictEncodedColumn();
+  int32_t uid = 0;
+  for (auto chunk: arrow_col->chunks()) {
+    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
+    for (int i=0; i < string_arr->length(); i++) {
+      auto str = string_arr->GetString(i);
+      if (result->dict.find(str) == result->dict.end()) {
+        result->dict[str] = uid; uid++;
+      }
+    }
+  }
+  result->column = new int32_t[table->num_rows()];
+  int j = 0; 
+  for (auto chunk: arrow_col->chunks()) {
+    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
+    for (int i=0; i < string_arr->length(); i++) {
+      auto str = string_arr->GetString(i);
+      result->column[j++] = result->dict[str];
+    }
+  }
+  return result;
+}
+StringColumn* read_string_column(std::shared_ptr<arrow::Table> &table, const std::string &column) {
+
+  //TODO: add all kinds of error handling
+  auto arrow_col = table->GetColumnByName(column);
+  StringColumn* result = new StringColumn();
+
+  // calculate the size of data
+  result->sizes = (int*)malloc(sizeof(int)*table->num_rows());
+  result->stringAddresses = (char**)malloc(sizeof(char*)*table->num_rows());
+  int data_size = 0;
+  int j=0;
+  for (auto chunk: arrow_col->chunks()) {
+    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
+    for (int i=0; i < string_arr->length(); i++) {
+      auto str = string_arr->GetString(i);
+      data_size += str.size();
+      result->sizes[j++] = str.size();
+    }
+  }
+  result->data = (char*)malloc(sizeof(char)*data_size);
+  j = 0;
+  char* straddr = result->data;
+  for (auto chunk: arrow_col->chunks()) {
+    auto string_arr = std::static_pointer_cast<arrow::LargeStringArray>(chunk);
+    for (int i=0; i < string_arr->length(); i++) {
+      auto str = string_arr->GetString(i);
+      result->stringAddresses[i] = straddr;
+      straddr += str.size();
+      for (auto c: str) {
+        result->data[j++] = c;
+      }
+    }
+  }
+  return result;
+}
+    
+arrow::Status read_parquet(std::string path_to_file, std::shared_ptr<arrow::Table> &table) {
+  arrow::MemoryPool* pool = arrow::default_memory_pool();
+  // open file
+  std::shared_ptr<arrow::io::RandomAccessFile> input;
+  ARROW_ASSIGN_OR_RAISE(input, arrow::io::ReadableFile::Open(path_to_file));
+  // initialize file reader
+  std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
+  ARROW_RETURN_NOT_OK(parquet::arrow::OpenFile(input, pool, &arrow_reader));
+
+  ARROW_RETURN_NOT_OK(arrow_reader->ReadTable(&table));
+
+  return arrow::Status::OK();
+}
+int getGridSize(size_t column_size, int thread_block_size) {
+  return (int)std::ceil(((float)column_size)/((float)thread_block_size));
+}
+ 
+std::shared_ptr<arrow::Table> getArrowTable(std::string path_to_file) {
+  std::shared_ptr<arrow::Table> lineitem_table;
+  arrow::Status st = read_parquet(path_to_file, lineitem_table);
+  if (st != arrow::Status::OK()) {
+    std::cerr << st.ToString();
+    return nullptr;
+  }
+  return lineitem_table;
+}
